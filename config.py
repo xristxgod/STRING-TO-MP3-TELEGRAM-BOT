b6a5d165e246a07b8b39a00b7de5b173dbf5edb1
Murad Mamedov <84931791+xristxgod@users.noreply.github.com>
2022-05-02 13:56:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import logging
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.join(ROOT_DIR, "files")
+
 if "files" not in os.listdir(ROOT_DIR):
     os.mkdir(BASE_DIR)
 
